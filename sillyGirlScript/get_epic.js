bc52878a1cd12c200f4d0caf97efc35826400863
suzhaoyong <su_zhao_yong@foxmail.com>
2022-01-08 14:36:09 +0800

    update

--- sillyGirlScript/get_epic.js
+++ sillyGirlScript/get_epic.js
@@ -2,6 +2,7 @@
 // [rule: Epic]
 // [cron: 30 11 * * *]
 /* 获取 epic 免费游戏
+    更多信息请查看 https://github.com/suzhaoyong/orzScript
  */
 request('https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=zh-CN&;;country=CN&allowCountries=CN', function (error, response, body) {
     var data = JSON.parse(body)
