5c9580557a23b5b084d69424e8be9c8931b79f62
suzhaoyong <su_zhao_yong@foxmail.com>
2022-02-21 16:39:32 +0800

    update

--- sillyGirlScript/Q绑查询-高级版.js
+++ sillyGirlScript/Q绑查询-高级版.js
@@ -1,10 +1,26 @@
 // Q绑查询 熊猫制作2021-12-10 QQ:1305759960 网站：bbs.xm08.cn
 // [rule: 查Q绑 ?]
-// [admin: false] 这里默认关闭了、开启true后除管理员账号外、群员不可查
+// [admin: true] 这里默认关闭了、开启true后除管理员账号外、群员不可查
 function main() {
-    var userID = GetUserID()
-    var time = (60) //禁言的时间 单位/秒
+    // var userID = GetUserID()
+    // var time = (60) //禁言的时间 单位/秒
     var qq = encodeURI(param(1))
+    if (qq.length === 11) {
+        var phone = qq
+        var data = request({
+            url: "https://zy.xywlapi.cc/qqcx?phone=" + phone ,
+        })
+        if (data.status === 200) {
+            qq = (data.qq)
+            var phonediqu = (data.phonediqu)
+            var lol = (data.lol)
+            var wb = (data.wb)
+            sendText("QQ："+qq + "\n归属地："+phonediqu+"\nLOL："+lol+"\n微博："+wb)
+        } else {
+            sendText("查无此人")
+        }
+    }
+    /*
     if (qq == 机器人QQ号) {
         sendText("查我？拉出去毙了") //这里设置机器人QQ、触发限制查询机器人QQ
         return
@@ -15,15 +31,19 @@ function main() {
         GroupBan(userID, time)
         return
     }
+    */
     var data = request({
-        url: "http://ovocc.top/chuanapi/chabangapi2.php?qq=" + qq ,
-        "dataType": "json"
+        url: "https://zy.xywlapi.cc/qqcx?qq=" + qq ,
     })
-    var phone = (data.phone)
-    var phonediqu = (data.phonediqu)
-    var lol = (data.lol)
-    var wb = (data.wb)
-    sendText("手机号："+phone+"\n归属地："+phonediqu+"\nLOL："+lol+"\n微博："+wb)
+    if (data.status === 200) {
+        var phone = (data.phone)
+        var phonediqu = (data.phonediqu)
+        var lol = (data.lol)
+        var wb = (data.wb)
+        sendText("手机号："+phone+"\n归属地："+phonediqu+"\nLOL："+lol+"\n微博："+wb)
+    } else {
+        sendText("查无此人")
+    }
     
        
 }
